088556a180d68b488873e444b0f6982346d686d7
bmawji3 <bmawji3@gatech.edu>
2016-11-23 17:26:59 -0600

    start work on login/logout functionality

--- phase3/app/general/views.py
+++ phase3/app/general/views.py
@@ -7,23 +7,34 @@ from .models import User
 cursor = mysql.connect().cursor()
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home', user="Admin")
+def home():
+    return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        if session.get('role') == 'Student':
+            return redirect(url_for('main_student'))
+        else:
+            return redirect(url_for('main_admin'))
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        query = 'SELECT * FROM user WHERE username=\'{}\' AND password=\'{}\''.format(username, password)
-        cursor.execute(query)
-        data = cursor.fetchone()
-        if (data):
+        query = 'SELECT username, userType FROM user WHERE username=\'{}\' AND password=\'{}\''.format(username, password)
+        result = cursor.execute(query)
+        if result:
+            data = cursor.fetchone()
             flash('Access Granted :)')
-            # user = User(username, password)
-            # login_user(user)
+            session['username'] = data[0]
+            session['role'] = data[1]
+            if data[1] == 'Student':
+                return redirect(url_for('main_student'))
+            else:
+                return redirect(url_for('main_admin'))
+    #         # user = User(username, password)
+    #         # login_user(user)
         else:
             flash('DENIED!!')
     else:
@@ -34,27 +45,39 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    if form.validate_on_submit():
-        # print('user: {} \nemail: {} \npassword: {} \npassword_confirm: {}'.format(form.username.data, form.email.data, form.password.data, form.password_confirm.data))
-        username = form.username.data
-        password = form.password.data
-        email = form.email.data
-        ''' TODO fix query for correct table '''
-        try:
-            query = 'INSERT INTO user (username, password) VALUES (\'{}\',  \'{}\')'.format(username, password)
-            cursor.execute(query)
-        except:
-            ''' TODO fix except clause for Integrity Error '''
-            flash('User already exists in DB!!')
-    else:
-        flash_errors(form)
+    # if form.validate_on_submit():
+    #     # print('user: {} \nemail: {} \npassword: {} \npassword_confirm: {}'.format(form.username.data, form.email.data, form.password.data, form.password_confirm.data))
+    #     username = form.username.data
+    #     password = form.password.data
+    #     email = form.email.data
+    #     ''' TODO fix query for correct table '''
+    #     try:
+    #         query = 'INSERT INTO user (username, password) VALUES (\'{}\',  \'{}\')'.format(username, password)
+    #         cursor.execute(query)
+    #     except:
+    #         ''' TODO fix except clause for Integrity Error '''
+    #         flash('User already exists in DB!!')
+    # else:
+    #     flash_errors(form)
     return render_template('register.html', title='Register', form=form)
 
 
-# @app.route('/logout', methods=['GET', 'POST'])
-# def logout():
+@app.route('/main-student', methods=['GET', 'POST'])
+def main_student():
+    return render_template('student/main_student.html', title='Main')
+
+
+@app.route('/main-admin', methods=['GET', 'POST'])
+def main_admin():
+    return render_template('admin/main_admin.html', title='Choose Functionality')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    session.pop('role', None)
 #     logout_user()
-#     return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 # @app.before_request
